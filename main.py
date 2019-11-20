9340a91f58303d6a4f74dccb3ae498b3ed478506
saller <seandavid.a@gmail.com>
2019-11-20 14:31:46 +0000

    Added figures folder and new functions for individual line plots

--- main.py
+++ main.py
@@ -6,18 +6,47 @@
 # %% Python Depdendencies
 import 	os
 import 	re
+import 	math
 import	pandas as pd 
 import 	numpy as np
 import 	matplotlib.pyplot as plt
 
+# %% Tube Line Colours
+# > Note: Hammersmith & City and Circle are a double colour due to being combined
+tubeColours 	= {
+	'Jubilee': 			"#A0A5A9",
+	'Northern': 		"#000000",
+	'Piccadilly': 		"#003688",
+	'Bakerloo': 		"#B36305",
+	'Metropolitan': 	"#9B0056",
+	'Victoria': 		"#0098D4",
+	'Central': 			"#E32017",
+	'District': 		"#00782A",
+	'Waterloo & City': 	"#95CDBA"	,
+	'Hammersmith & City and Circle': "#F3A9BB#FFD300"
+}
+
+# %% Peak time definitions
+peakTimes 	= {
+	'amStart':	3,	# 06:30
+	'amStop': 	5,	# 09:30
+	'pmStart': 	12,	# 16:00
+	'pmStop': 	15	# 19:00
+}
+
 # %% Function Definitions
+
+def roundTen(n):
+	# Round up number to the nearest multiple of 10
+	return int(math.ceil(n / 10.0)) * 10
+
 def loadData(file):
-	# Load the data from supplied CSV file and transform columns into appropriate time axes
+	# > Load the data from supplied CSV file and transform columns into appropriate time axes
 	df 			= pd.read_csv(file)
 	timePoints 	= list(df.columns[1:])
-	# Isolate the individual start and end for each time point
-	newPoints 	= []
-	newPoints.append('Line')
+	# > Isolate the individual start and end for each time point
+	newPoints 		= []
+	formattedTime 	= []
 	for point in timePoints:
 		if '0000-' in point:
 			t1 	= int(24)
@@ -28,20 +57,110 @@ def loadData(file):
 		#else:
 		#	t2 	= int(re.search(r'(?<=-)(.*)(?<!0)',point).group().lstrip("0"))
 		newPoints.append(t1)
-	# Edit frame
-	newFrame = df
-	newFrame.columns = newPoints
-	newFrame.iloc[1:,1:] = newFrame.iloc[1:,1:] * 100
-	# Return the frame
-	return newFrame
+		if len(str(t1)) == 1:
+			formattedTime.append('0'+str(t1)+':00')
+		else:
+			formattedTime.append(str(t1)+':00')
+	# > Additional time to account for day shift
+	formattedTime.append("(+1) 01:00")
+	# > Edit frame
+	newFrame 			= df
+	newCol 				= newPoints
+	newCol 				= [float(x)-3.5 for x in newCol]
+	colList 			= ['Line']
+	colList.extend(newCol)
+	newFrame.columns 	= colList
+	newFrame.iloc[:,1:] = newFrame.iloc[:,1:] * 100
+	# > Return the frame
+	return newFrame, formattedTime
 
-def summaryLinePlot(frame):
+def individualLinePlot(frame, peakTimes, xLabels, line):
+	# Create line plot, with time as x-azis, for a specific line
+	# > Load the tube information
+	tubeFrame 	= frame[frame['Line'] == line]
+	tubeColor 	= tubeColours[line]
+	tubeX 		= tubeFrame.columns[1:]
+	tubeY 		= tubeFrame.iloc[0,1:]
+	# > Create the plot
+	plt.plot(tubeX,tubeY, 
+		color 			= tubeColor, 
+		linewidth 		= 3,
+		marker 			= 'o',
+		markeredgecolor = tubeColor,
+		markeredgewidth = 2,
+		markerfacecolor = 'white',
+	)
+	# > X Tick Labels
+	plt.xticks(range(1,len(xLabels)+1,1), xLabels, rotation = "vertical")
+	# > Ensure correct scaling of y axis limits
+	if roundTen(max(tubeY)) < 100:
+		yMax 	= 100
+	else:
+		yMax = roundTen(max(tubeY))
+	# > Plot limit and margin adjustments
+	plt.ylim((0,yMax))
+	plt.subplots_adjust(bottom=0.2)
+	# > Create highlighted regions for peak times
+	plt.axvspan(
+		peakTimes['amStart'], 
+		peakTimes['amStop'],
+		color 	= 'red',
+		alpha 	= 0.25) 	# AM Peak
+	plt.axvspan(
+		peakTimes['pmStart'], 
+		peakTimes['pmStop'],
+		color 	= 'red',
+		alpha 	= 0.25) 	# PM Peak
+	return plt.show()
+
+def multipleLinePlots(frame):
+	# Create line plots, with time as x-azis, for the capacity of each individual line
+	# > Identify all the lines
+	tubeLines 	= list(set(frame['Line']))
+	# > Create and save a plot for each tube line
+	for tube in tubeLines:
+		# > Load the tube information
+		tubeFrame, xLabels 	= frame[frame['Line'] == tube]
+		tubeColor 	= tubeColours[tube]
+		tubeX 		= tubeFrame.columns[1:]
+		tubeY 		= tubeFrame.iloc[0,1:]
+		# > Create the plot
+		plt.plot(testX,testY, 
+			color 			= tubeColor, 
+			linewidth 		= 3,
+			marker 			= 'o',
+			markeredgecolor = tubeColor,
+			markeredgewidth = 2,
+			markerfacecolor = 'white',
+		)
+
+# def summaryLinePlot(frame):
 	# Create a line plot, with time as x-axis, for the capacity of all lines
-	
-	
-# def individualLinePlot(frame):
-	# Create line plots, with time as x-azis, for the capacity of each individual line	
 
 # %% Run Workflow
-frame 	= loadData('MQ2019_19838_TubeCapacity.csv')
-print(frame.head())
+# > Isolate frame and xTick Labels
+frame, xLabels = loadData('MQ2019_19838_TubeCapacity.csv')
+individualLinePlot(frame, peakTimes, xLabels, 'Northern')
+# Single graphics testing
+testFrame 	= frame[frame['Line'] == 'Metropolitan']
+testX 		= testFrame.columns[1:]
+testY 		= testFrame.iloc[0,1:]
+plt.plot(testX,testY, 
+	color = tubeColours['Metropolitan'], 
+	linewidth = 3,
+	marker = 'o',
+	markeredgecolor = tubeColours['Metropolitan'],
+	markeredgewidth = 2,
+	markerfacecolor = 'white',
+	)
+#plt.margins(0.2)
+plt.xticks(range(1,len(xLabels)+1,1), xLabels, rotation = "vertical")
+
+if roundTen(max(testY)) < 100:
+	yMax 	= 100
+else:
+	yMax = roundTen(max(testY))
+
+plt.ylim((0,yMax))
+plt.subplots_adjust(bottom=0.2)
+plt.show()
